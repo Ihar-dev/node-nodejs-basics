4e60b57e8fc2c50622cd902596a413ff0dae9d0a
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-02 22:27:19 +0300

    feat: edit copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,22 +1,49 @@
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 import { access } from 'fs/promises';
-import { appendFile } from 'fs/promises';
+import { mkdir } from 'fs/promises';
+import { copyFile } from 'fs/promises';
+import fs from 'fs';
+const fsPromises = fs.promises;
 
 export const copy = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   const prevDir = path.join(__dirname, 'files');
-  const nextDir = path.join(__dirname, 'files_copy');
+  const newDir = path.join(__dirname, 'files_copy');
   const errMsg = () => console.log(new Error('\x1b[31m FS operation failed'));
+
+  const copyDirectory = () => {
+    try {
+      const files = await fsPromises.readdir(prevDir);
+      for (let file of files) {
+        let prevFilePath = path.join(prevDir, file);
+        let nextFilePath = path.join(nextDir, file);
+        copyFile(prevFilePath, nextFilePath);
+      }
+    } catch (err) {
+      if (err) throw err;
+    }
+  };
+
+  const makeDirectory = async () => {
+    try {
+      await mkdir(newDir, {
+        recursive: true
+      });
+      copyDirectory();
+    } catch (err) {
+      if (err) throw err;
+    }
+  };
+
   try {
     await access(prevDir);
-    console.log('found');
     try {
-      await access(nextDir);
+      await access(newDir);
       errMsg();
     } catch {
-      // appendFile(filePath, 'I am fresh and young');
+      makeDirectory();
     }
   } catch {
     errMsg();
