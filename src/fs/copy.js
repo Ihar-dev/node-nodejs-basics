80c439ecee9a302cb9d9132dd2c652f9c888ef2c
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-02 21:55:37 +0300

    feat: edit copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,18 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { access } from 'fs/promises';
+import { appendFile } from 'fs/promises';
+
 export const copy = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const prevDir = path.join(__dirname, 'files');
+  const nextDir = path.join(__dirname, 'files_copy');
+  try {
+    await access(prevDir);
+    console.log('found');
+  } catch {
+    console.log(new Error('\x1b[31m FS operation failed'));
+  } 
 };
+copy();
