3a6450dcaeaecee17785217d05006c4f0cb9c7fc
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-02 22:03:49 +0300

    feat: edit copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -8,11 +8,18 @@ export const copy = async () => {
   const __dirname = path.dirname(__filename);
   const prevDir = path.join(__dirname, 'files');
   const nextDir = path.join(__dirname, 'files_copy');
+  const errMsg = () => console.log(new Error('\x1b[31m FS operation failed'));
   try {
     await access(prevDir);
     console.log('found');
+    try {
+      await access(nextDir);
+      errMsg();
+    } catch {
+      // appendFile(filePath, 'I am fresh and young');
+    }
   } catch {
-    console.log(new Error('\x1b[31m FS operation failed'));
+    errMsg();
   } 
 };
 copy();
