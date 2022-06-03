3f95002deec785378e3182a5ce46a932cb89f262
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-03 16:39:21 +0300

    feat: edit read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,28 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { access } from 'fs/promises';
+import { open } from 'fs/promises';
+
+const readFile = async (fileToReadPath) => {
+  const fd = await open(fileToReadPath);
+  const myReadStream = fd.createReadStream();
+  myReadStream.on('data', chunk => {
+    const textData = '' + Buffer.from(chunk);
+    process.stdout.write(textData + '\n');
+    /* const textData = Buffer.from(chunk).toString();
+    console.log(textData); */                                // another option
+  });
+}
+
 export const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fileToReadPath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    await access(fileToReadPath);
+    readFile(fileToReadPath);  
+  } catch {
+    console.log(new Error('\x1b[31m FS operation failed'));
+  }
 };
