a1bccc7912ad7af55f69a5bfd03839e84c436852
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-05 01:37:12 +0300

    feat: edit calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,18 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+const { createHash } = await import('crypto');
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const input = createReadStream(filePath);
+  
+  input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
+
+calculateHash();
