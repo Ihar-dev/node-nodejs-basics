15438c6ffe00e0fe7434ab10e85ac881efd8af55
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-05 17:40:37 +0300

    feat: edit decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,24 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import * as zlib from 'zlib';
+import fs from 'fs';
+import { open } from 'fs/promises';
+
 export const decompress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fileToReadPath = path.join(__dirname, 'files', 'archive.gz');
+  const fileToWritePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const myWritableStream = fs.createWriteStream(fileToWritePath);
+  const fd = await open(fileToReadPath);
+  const myReadStream = fd.createReadStream();
+  
+  myReadStream.on('data', chunk => {
+    zlib.unzip(chunk, function(err, result) {
+      if (err) return console.log('error ' + err);
+      myWritableStream.write(result);
+    });
+  });
 };
+
+decompress();
