2324bc93ca5122fb97ab33d9617138c521599a03
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-05 14:02:30 +0300

    feat: edit read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,17 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { open } from 'fs/promises';
+
 export const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fileToReadPath = path.join(__dirname, 'files', 'fileToRead.txt');
+  const fd = await open(fileToReadPath);
+  const myReadStream = fd.createReadStream();
+  myReadStream.on('data', chunk => {
+    const textData = Buffer.from(chunk).toString();
+    process.stdout.write(textData + '\n');
+  });
 };
+
+read();
