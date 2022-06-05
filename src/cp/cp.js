d4c564a0cac948ec965d3ed36d0b21f59b5b9063
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-05 23:49:55 +0300

    feat: edit cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -3,11 +3,18 @@ import { fileURLToPath } from 'url';
 import * as path from 'path';
 
 export const spawnChildProcess = async (args) => {
+  args.splice(0, 2);
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   const filePath = path.join(__dirname, 'files', 'script.js');
-  const child = childProcess.fork(filePath, args);
-  console.log('Enter your text or press "ctrl + c" to quit\n');
+  const child = childProcess.fork(filePath, args, { silent: true });
+  process.stdout.write('Enter your text or press "ctrl + c" to quit\n');
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+  child.stdout.on("data", data => {
+    const msg = `Received from child stdout: ${data}`;
+    process.stdout.write(msg);
+  });
 };
 
 spawnChildProcess(process.argv);
