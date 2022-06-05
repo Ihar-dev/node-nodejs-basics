d132f6f8d922a3233b0e233bfe05eb0c5fa59344
Ihar Kozik <ihar.kozik@gmail.com>
2022-06-05 19:40:22 +0300

    feat: edit main.js and worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,10 +1,34 @@
 import * as os from 'os';
+import { Worker } from 'worker_threads';
 
 export const performCalculations = async () => {
   const CPUCores = os.cpus();
-  console.log(CPUCores);
-  CPUCores.forEach(core => {
-    console.log(core);
+  const outputArray = [];
+  let number = 10;
+  const threadsNumber = CPUCores.length;
+  let currentThreadsNumber = 0;
+  
+  CPUCores.forEach((core, index) => {
+    const obj = {
+      status: '',
+      data: null,
+    };
+    outputArray.push(obj);
+    const worker = new Worker('./worker.js', {
+      workerData: number,
+    });
+    worker.on('message', msg => {
+      outputArray[index].status = 'resolved';
+      outputArray[index].data = msg;
+    });
+    worker.on('error', err => {
+      outputArray[index].status = 'error';
+    });
+    worker.on('exit', () => {
+      currentThreadsNumber++;
+      if (currentThreadsNumber === threadsNumber) console.log(outputArray);
+    });
+    number++;
   });
 };
 
